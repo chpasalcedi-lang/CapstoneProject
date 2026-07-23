9a10c9c1b9351fd3bbfd707005b6a7e0b18270f1
Lyle Valderrama <crumbleee05@gmail.com>
2026-07-24 01:59:48 +0800

    last_last_commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../client/.env') });
+dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 const app = express();
 

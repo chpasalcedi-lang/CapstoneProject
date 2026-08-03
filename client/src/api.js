34874d8d2b41f723150bda7cea99d071b051686f
chpasalcedi-lang <chpa.salcedo.ui@phinmaed.com>
2026-08-03 18:35:06 +0800

    Last commit

--- client/src/api.js
+++ client/src/api.js
@@ -11,8 +11,29 @@ if (!apiUrl.startsWith('http://') && !apiUrl.startsWith('https://')) {
   apiUrl = 'https://capstoneproject-bqso.onrender.com/api';
 }
 
+const fallbackApiUrl = 'https://capstoneproject-bqso.onrender.com/api';
 const apiClient = axios.create({
   baseURL: apiUrl,
+});
+
+apiClient.interceptors.request.use((config) => {
+  if (!config.baseURL || !/^https?:\/\//.test(config.baseURL) || config.baseURL.toLowerCase() === 'http://' || config.baseURL.toLowerCase() === 'https://') {
+    config.baseURL = fallbackApiUrl;
+  }
+
+  if (typeof config.url === 'string') {
+    const normalizedUrl = config.url.trim();
+
+    if (/^https?:\/\/delete_room(\/.*)?$/i.test(normalizedUrl)) {
+      config.url = normalizedUrl.replace(/^https?:\/\/delete_room/i, '/delete_room');
+    } else if (/^delete_room(\/.*)?$/i.test(normalizedUrl)) {
+      config.url = normalizedUrl.replace(/^delete_room/i, '/delete_room');
+    } else {
+      config.url = normalizedUrl;
+    }
+  }
+
+  return config;
 });
 
 export default apiClient;

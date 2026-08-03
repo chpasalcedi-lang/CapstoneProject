c4caa7c0f77d5c918fbbd6abc945a44ab6ef5724
chpasalcedi-lang <chpa.salcedo.ui@phinmaed.com>
2026-08-03 19:33:10 +0800

    Last commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://capstoneproject-bqso.onrender.com/api';
+const apiUrl = import.meta.env.VITE_API_URL || '/api';
 const apiClient = axios.create({
   baseURL: apiUrl,
 });
